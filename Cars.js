f8354a019903d9dc4a8c01f8eca44034ae053ad3
Kazae67 <Kazae.zaeka@gmail.com>
2022-12-08 15:52:57 +0100

    début exercice 13 (Cars)

---
+++ Cars.js
@@ -0,0 +1,9 @@
+class Car {
+    constructor(marque, modele, nbPortes, vitesseActuelle, statut) {
+        this.marque = marque;
+        this.modele = modele;
+        this.nbPortes = nbPortes;
+        this.vitesseActuelle = vitesseActuelle;
+        this.statut = statut;
+    }
+}
