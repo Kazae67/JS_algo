8cb3d0cbb832d8715e70e29cb3633bf95273f48d
Kazae67 <Kazae.zaeka@gmail.com>
2022-12-08 16:37:53 +0100

    cars > getters appliqué

--- Cars.js
+++ Cars.js
@@ -1,15 +1,40 @@
+// Class 🏠🧔👩
 class Car {
     marque;
     modele;
     nbPortes;
     vitesseActuelle;
     statut;
-
-    constructor(marque, modele, nbPortes, vitesseActuelle, statut) {
+    
+    // Construt 🔒
+    constructor(marque, modele, nbPortes) {
         this.marque = marque;
         this.modele = modele;
         this.nbPortes = nbPortes;
         this.vitesseActuelle = 0;
         this.statut = false;
     }
+
+    // Getters 🔑
+    get marque(){
+        return this.marque();
+    }
+
+    get modele(){
+        return this.modele();
+    }
+    
+    get nbPortes(){
+        return this.nbPortes();
+    }
+
+    get vitesseActuelle(){
+        return this.vitesseActuelle();
+    }
+
+    get statut() {
+        return this.statut() ? "démarré" : "arrêté";
+    }
+
+    
 }
